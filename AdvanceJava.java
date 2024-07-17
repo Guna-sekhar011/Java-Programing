9c523b30beab0b8da1a9a59f0208cfbbc142d764
Thokala Guna Sekhar <114064761+Guna-sekhar011@users.noreply.github.com>
2024-07-17 13:04:57 +0530

    JDBC

---
+++ AdvanceJava.java
@@ -0,0 +1,21 @@
+package JdbcJava;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class AdvanceJava {
+
+	public static void main(String[] args) {
+		String url ="jdbc:mysql://localhost:3306/advancejava";
+		String username = "root";
+		String password = "root";
+		try {
+			Connection con = DriverManager.getConnection(url, username, password);
+			System.out.println("Success...!");
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
