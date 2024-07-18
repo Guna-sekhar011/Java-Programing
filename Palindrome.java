1c06fe2b7887ed8ae6a0dada3436e870069eba62
Thokala Guna Sekhar <114064761+Guna-sekhar011@users.noreply.github.com>
2024-07-18 12:17:50 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Palindrome{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String str2 ="";
+        for(int i=str.length()-1;i>=0;i--){
+            str2=str2+str.charAt(i);
+        }
+        System.out.println(str2);
+    
+        int res = str.compareTo(str2);
+        if(res==0){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    
+    }
+}
